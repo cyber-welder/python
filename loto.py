cae8bc0ebee39b4e4b3191b34cae227f377e06d2
Cyber-Welder <cyber-welder@ya.ru>
2021-02-06 23:23:00 +0800

    Добавлена сложность

--- loto.py
+++ loto.py
@@ -61,19 +61,20 @@ from random import shuffle, sample, randint
 
 class Gamer:
     """ Игрок. Принимает имя игрока. Методом pull_card получает карточку (Card) """
+
     def __init__(self, name):
         self.name = name
-        self.gamer_card = []
-        self.answer = ''
-        self.true_answer = True
-        self.end_card = False
+        self.gamer_card = []    # карточка игрока
+        self.answer = ''    # ответ
+        self.true_answer = True  # правильность ответа
+        self.end_card = False   # заполненость карточки
 
     def pull_card(self, gamer_card):
         self.gamer_card = gamer_card
 
 
 class Card:
-    """ Карточка. Генерируется при создании"""
+    """ Карточка лото. Генерируется при создании"""
     def __init__(self):
         self.cells, cells = [], []
         nums = [['1', '2', '3', '4', '5', '6', '7', '8', '9'],
@@ -85,8 +86,8 @@ class Card:
                 ['60', '61', '62', '63', '64', '65', '66', '67', '68', '69'],
                 ['70', '71', '72', '73', '74', '75', '76', '77', '78', '79'],
                 ['80', '81', '82', '83', '84', '85', '86', '87', '88', '89', '90']]
-
-        unique_nums = [sample(dec, 3) for dec in nums]
+        # далее весьма мутный код формирования карточки по правилам лото
+        unique_nums = [sample(dec, 3) for dec in nums]  # уникальные значения не более трех в каждом десятке
         for i in range(3):
             cells.append([n[i] for n in unique_nums])
             empty = sample(cells[i], 4)
@@ -95,11 +96,12 @@ class Card:
             self.cells.extend(cells[i])
 
     def __str__(self):
-        return '\n'.join(['\t'.join(list(map(str, self.cells[9*i:9*i+9]))) for i in range(3)])
+        return '\n'.join(['\t'.join(list(map(str, self.cells[9 * i:9 * i + 9]))) for i in range(3)])
 
 
 class Bag:
     """ Мешок с бочонками (1-90). Метод pull_barrel вытаскивает бочонки по одному пока они не кончатся """
+
     def __init__(self):
         self.barrels = list(map(str, range(1, 91)))
         shuffle(self.barrels)
@@ -111,6 +113,7 @@ class Bag:
 
 class Game:
     """ Игра. Принимает сложность игры 0-3, мешок (Bag) и игроков (Gamer) """
+
     def __init__(self, diff, bag, *gamers):
         self.gamers = gamers
         self.bag = bag
@@ -123,7 +126,7 @@ class Game:
     def _end_card(self):
         """ Проверяет заполнение карточек игроков.
          Возвращает строку с победителями или пустую если их нет"""
-        result, draw = '',  0
+        result, draw = '', 0
         for gamer in self.gamers:
             gamer.end_card = True
             for i in gamer.gamer_card.cells:
@@ -131,7 +134,7 @@ class Game:
                     gamer.end_card = False
             draw += gamer.end_card
             if gamer.end_card:
-                result += f'Победил игрок {gamer.name}'.center(len(gamer.name)+17, ' ').center(34, '-') + '\n'
+                result += f'Победил игрок {gamer.name}'.center(len(gamer.name) + 17, ' ').center(34, '-') + '\n'
 
         if len(result):
             if draw > 1:
@@ -153,7 +156,7 @@ class Game:
                 gamer.true_answer = False if gamer.answer.lower() == 'y' else True
             draw += gamer.true_answer
             if not gamer.true_answer:
-                result += f'Игрок {gamer.name} проиграл'.center(len(gamer.name)+17, ' ').center(34, '-') + '\n'
+                result += f'{gamer.name} дал не верный ответ'.center(len(gamer.name) + 22, ' ').center(34, '-') + '\n'
 
         if len(result):
             if not draw:
@@ -177,19 +180,20 @@ class Game:
                 self.gamers[1].answer = 'y' if self.gamers[1].answer == 'n' else 'n'
             print()
             print('=' * 34)
-
+            # Проверка правильности ответов
             result = self._check_answer(barrel)
             if len(result):
-                return result
-
+                break
+            # Проверка заполнения карточки
             result = self._end_card()
             if len(result):
-                return result
+                break
+        print(result)
 
 
 bag = Bag()
 
-gamer1 = Gamer('Puper User') #Gamer(input('Введите свое имя: '))
+gamer1 = Gamer(input('Введите свое имя: '))
 gamer2 = Gamer('Compukter')
 
 gamer1.pull_card(Card())
@@ -197,10 +201,4 @@ gamer2.pull_card(Card())
 
 game = Game(input('Введите сложность игры числом (сложнее - легче, 3 - 0): '), bag, gamer1, gamer2)
 
-print(game.start())
-
-
-
-
-
-
+game.start()
